c6dbe0e6fefde927c233b96832b72ff305371d8f
Jana Abouemira <60305219@udst.edu.qa>
2026-04-10 18:48:52 +0300

    Update cleaning.py

--- scripts/cleaning.py
+++ scripts/cleaning.py
@@ -1,1 +1,10 @@
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.getOrCreate()
+
+raw_path = "abfss://raw@jana60305219.dfs.core.windows.net/AmesHousing.csv"
+df = spark.read.csv(raw_path, header=True, inferSchema=True)
+
+processed_path = "abfss://processed@jana60305219.dfs.core.windows.net/AmesHousing_cleaned"
+df.write.mode("overwrite").parquet(processed_path)
 
