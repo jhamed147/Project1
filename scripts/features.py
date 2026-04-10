7c5575e0663b50762fe408a4488f945489ae2954
Jana Abouemira <60305219@udst.edu.qa>
2026-04-10 19:30:11 +0300

    Update features.py

--- scripts/features.py
+++ scripts/features.py
@@ -1,19 +1,21 @@
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import col
 from pyspark.ml.feature import VectorAssembler, StandardScaler
 
 spark = SparkSession.builder.getOrCreate()
 
-processed_path = "abfss://processed@jana60305219.dfs.core.windows.net/AmesHousing_cleaned"
+processed_path = "abfss://processed@jana60305219.dfs.core.windows.net/AmesHousing_processed"
 df = spark.read.parquet(processed_path)
 
-assembler = VectorAssembler(
-    inputCols=["Gr_Liv_Area", "Garage_Area", "Total_Bsmt_SF", "Year_Built"],
-    outputCol="numerical_features"
-)
-df_num = assembler.transform(df)
+df = df.withColumn("Age", col("Yr_Sold") - col("Year_Built"))
 
-scaler = StandardScaler(inputCol="numerical_features", outputCol="numerical_scaled")
-df_scaled = scaler.fit(df_num).transform(df_num)
+feature_cols = ["Gr_Liv_Area", "Garage_Area", "Total_Bsmt_SF", "Age", "Overall_Qual"]
+
+assembler = VectorAssembler(inputCols=feature_cols, outputCol="numerical_features")
+df = assembler.transform(df)
+
+scaler = StandardScaler(inputCol="numerical_features", outputCol="features")
+df = scaler.fit(df).transform(df)
 
 features_path = "abfss://features@jana60305219.dfs.core.windows.net/AmesHousing_features"
-df_scaled.write.mode("overwrite").parquet(features_path)
+df.write.mode("overwrite").parquet(features_path)
